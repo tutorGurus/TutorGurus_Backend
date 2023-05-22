a2210b14a0a0cfbf8bbfa541334311f4c6357115
Avocado <cl4m3861026@gmail.com>
2023-05-22 09:25:57 +0800

    feat : env

--- src/middleware/passportValidate.js
+++ src/middleware/passportValidate.js
@@ -1,7 +1,8 @@
 const passport = require('passport');
 const GoogleStrategy = require( 'passport-google-oauth20' ).Strategy;
 const User = require('../models/userModel');
-passport.use(new GoogleStrategy({
+if(process.env.NODE_ENV.trim() === 'dev'){
+    passport.use(new GoogleStrategy({
         clientID : process.env.GOOGLE_AUTH_CLIENTID,
         clientSecret : process.env.GOOGLE_AUTH_CLIENT_SECRET,
         callbackURL : "http://localhost:8000/user/v1/google/callback"
@@ -21,5 +22,28 @@ passport.use(new GoogleStrategy({
         }
     }
 ))
+  } else {
+        passport.use(new GoogleStrategy({
+            clientID : process.env.GOOGLE_AUTH_CLIENTID,
+            clientSecret : process.env.GOOGLE_AUTH_CLIENT_SECRET,
+            callbackURL : "https://tutorgurus-backend.onrender.com/user/v1/google/callback"
+        },
+        async function(accessToken, refreshToken, profile, cb) {
+            try {
+                const user = await User.findOrCreate(
+                {
+                    googleId: profile.id,
+                    name: profile.displayName, 
+                    email: profile.emails[0].value,
+                }
+                );
+                return cb(null, user);
+            } catch (err) {
+                return cb(err);
+            }
+        }
+    ))
+  }
+
 
 module.exports = passport;
